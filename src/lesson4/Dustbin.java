30e64829f3d8b88eb6d1aa6401a2ac5abac829df
alexbor2015 <aborisenko2015@gmail.com>
2019-04-17 19:35:46 +0300

    alpha 1.2

--- src/lesson4/Dustbin.java
+++ src/lesson4/Dustbin.java
@@ -4,18 +4,28 @@ import java.util.Scanner;
 
 public class Dustbin {
     public static void main(String[] args) {
-        System.out.println("Please, type your command (insert money  [F] or make a foto [M]");
-        Scanner in = new Scanner(System.in);
-        String command = in.nextLine();
+        int credit = 0;
+        while(true) {
+            System.out.println("Please, type your command (insert money  [F] or make a foto [M]");
+            Scanner in = new Scanner(System.in);
+            String command = in.nextLine();
 
-        switch(command){
-            case "F":
-                System.out.println("Smile!:)");
-                break;
-            case "M":
-                System.out.println("Where are my money , Lebowski?");
-            default :
-                System.out.println("Err[01]: Invalid command");
+            switch (command) {
+                case "F":
+                    System.out.println("Smile!:)");
+                    if (credit > 0) {
+                        credit--;
+                    } else {
+                        System.out.println("Err[02]: Not enough money");
+                    }
+                    break;
+                case "M":
+                    System.out.println("Where are my money , Lebowski?");
+                    credit++;
+                    break;
+                default:
+                    System.out.println("Err[01]: Invalid command");
+            }
         }
     }
 }
