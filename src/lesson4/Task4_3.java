931146d6d503458fc519b110cf356f01e0f327ef
alexbor2015 <aborisenko2015@gmail.com>
2019-04-19 19:08:26 +0300

    lesson4 release

---
+++ src/lesson4/Task4_3.java
@@ -0,0 +1,23 @@
+package lesson4;
+
+import java.util.Scanner;
+
+/*
+Task
+Используя цикл while, рассчитайте для введенного числа его факториал. Факториал числа n - это произведение всех натуральных чисел от 1 до n.
+*/
+public class Task4_3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter you number");
+        int i = 1;
+        int n = in.nextInt();
+        int ind = n;
+
+        while (i < ind){
+             n *= i;
+             i++;
+        }
+        System.out.println("The solution for " + ind + " is " + n);
+    }
+}
