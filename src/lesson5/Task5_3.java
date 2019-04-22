d4e771be2a5cb885d4ab391a28118da119a22500
burtest <sannnna@bk.ru>
2019-04-22 19:08:55 +0300

    alpha 5.3

---
+++ src/lesson5/Task5_3.java
@@ -0,0 +1,23 @@
+package lesson5;
+
+import java.util.Scanner;
+
+/*
+Task
+Имеется целое число, введенное в клавиатуры, определить является ли это число простым, т.е. делится без остатка только на 1 и себя.
+ */
+public class Task5_3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Checked number?");
+        int num = in.nextInt();
+
+        for(int i = 1; i < num; i++){
+            if (num % i == 0){
+                System.out.println("Nope");
+                System.exit(0);
+            }
+        }
+        System.out.println("Yay");
+    }
+}
