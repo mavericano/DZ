652f0e3000e70bc886e87bdb44d957708b790512
burtest <sannnna@bk.ru>
2019-04-24 21:07:55 +0300

    release 6.2 + addons

--- src/lesson7/OutOfMemoryErr.java
+++ src/lesson7/OutOfMemoryErr.java
@@ -1,16 +1,33 @@
 package lesson7;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
 public class OutOfMemoryErr {
-    static Item [] items = new Item[Integer.MAX_VALUE - Short.MAX_VALUE];
+
+    static List<Item> items = new ArrayList<>();
+
 
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-
+        long t1 = System.currentTimeMillis();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
         }
+
+        for (int i = 0; i <= 10000000; i++) {
+            items.add(new Item(i, String.valueOf(i)));
+        }
+        long t2 = System.currentTimeMillis();
+        System.out.println(t2 - t1);
     }
+
 }
 
-class Item{
+class Item {
     long aLong;
     String string;
 
