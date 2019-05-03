bb76f7741f0ac6858000cc954d7996d03deb9e72
burtest <sannnna@bk.ru>
2019-05-03 21:17:07 +0300

    alpha 10.1

---
+++ src/lesson11/Test.java
@@ -0,0 +1,11 @@
+package lesson11;
+
+import java.util.ArrayList;
+
+public class Test {
+    ArrayList<Integer> arrL= new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+
+    }
+}
