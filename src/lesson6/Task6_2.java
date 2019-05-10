e95b118776a1be725096ff1930083089f2c0860f
mavericano <scoop.doubleup@gmail.com>
2019-05-10 23:00:51 +0300

    butterfly added

--- src/lesson6/Task6_2.java
+++ src/lesson6/Task6_2.java
@@ -13,6 +13,13 @@ public class Task6_2 {
 
         int [][] arr = new int[a][a];
 
+        for (int i = 0; i <= (a/2) + 1; i++) {
+            for(int j = i; j <= a - i - 1; j++){
+                arr[i][j] = 1;
+                arr[a - i -1][a - j - 1] = 1;
+            }
+        }
+
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 System.out.print(arr[i][j]);
